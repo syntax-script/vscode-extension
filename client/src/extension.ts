9d3f79e049955ed8c651e37e99bb73980393ffd5
efekos <hefeolgun10@gmail.com>
2024-04-07 17:52:15 +0300

    setup

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -11,13 +11,10 @@ import {
 let client: LanguageClient;
 
 export function activate(context: ExtensionContext) {
-  // The server is implemented in node
   const serverModule = context.asAbsolutePath(
     path.join("server", "out", "server.js")
   );
 
-  // If the extension is launched in debug mode then the debug server options are used
-  // Otherwise the run options are used
   const serverOptions: ServerOptions = {
     run: { module: serverModule, transport: TransportKind.ipc },
     debug: {
@@ -26,25 +23,20 @@ export function activate(context: ExtensionContext) {
     },
   };
 
-  // Options to control the language client
   const clientOptions: LanguageClientOptions = {
-    // Register the server for all documents by default
-    documentSelector: [{ scheme: "file", language: "syx" },{scheme:'file', language:'sys'}],
+    documentSelector: [{ scheme: "file", language: "syx" }, { scheme: 'file', language: 'sys' }],
     synchronize: {
-      // Notify the server about file changes to '.clientrc files contained in the workspace
       fileEvents: workspace.createFileSystemWatcher("**/.clientrc"),
-    },
+    }
   };
 
-  // Create the language client and start the client.
   client = new LanguageClient(
-    "REPLACE_ME language-server-id",
-    "REPLACE_ME language server name",
+    "syntax-script language-server-id",
+    "syntax-script language server name",
     serverOptions,
     clientOptions
   );
 
-  // Start the client. This will also launch the server
   client.start();
 }
 
