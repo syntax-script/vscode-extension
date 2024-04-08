b34bb5f7661a0c183d23e9d9d1395a0a2d313e09
efekos <hefeolgun10@gmail.com>
2024-04-08 14:59:18 +0300

    imports-keyword

--- server/src/dictionary/rules.ts
+++ server/src/dictionary/rules.ts
@@ -7,5 +7,15 @@ export default
     {
         name:'function-value-return-keyword',
         type:'keyword'
+    },
+    {
+        name:'imports-keyword',
+        type:'keyword'
     }
-]
+] as Rule[];
+type RuleType = 'keyword'|'boolean';
+
+interface Rule {
+    name: string;
+    type: RuleType
+}
