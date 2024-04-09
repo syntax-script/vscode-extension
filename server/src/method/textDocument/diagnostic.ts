c8aecbcd7a069037e8e4d7e93dc0dba6b24593ad
efekos <hefeolgun10@gmail.com>
2024-04-10 01:49:14 +0300

    add debug items and use the method with content

--- server/src/method/textDocument/diagnostic.ts
+++ server/src/method/textDocument/diagnostic.ts
@@ -1,6 +1,8 @@
 import { DiagnosticSeverity, DocumentDiagnosticReportKind, FullDocumentDiagnosticReport, createSyntaxScriptDiagnosticReport } from "@syntaxs/compiler";
 import { TextDocumentIdentifier, documents } from "../../documents";
 import { RequestMessage } from "../../types";
+
+
 interface DiagnosticParams {
     textDocument: TextDocumentIdentifier;
 }
@@ -9,9 +11,9 @@ export function diagnostic(message: RequestMessage): FullDocumentDiagnosticRepor
 
     const params = message.params as DiagnosticParams;
     const content = documents.get(params.textDocument.uri);
-    if (!content) return {kind:DocumentDiagnosticReportKind.Full,items:[{range:{start:{character:1,line:0},end:{character:2,line:0}},message:'Test',severity:DiagnosticSeverity.Error}]};
+    if (!content) return {kind:DocumentDiagnosticReportKind.Full,items:[{range:{start:{character:1,line:0},end:{character:2,line:0}},message:content??'n content',severity:DiagnosticSeverity.Error}]};
 
-    const report = createSyntaxScriptDiagnosticReport(params.textDocument.uri);
+    const report = createSyntaxScriptDiagnosticReport(params.textDocument.uri,content);
 
-    return report;
+    return {kind:DocumentDiagnosticReportKind.Full,items:report?report.items:[{message:'No report',range:{end:{character:0,line:1},start:{character:0,line:0}},severity:DiagnosticSeverity.Warning}]};
 }
