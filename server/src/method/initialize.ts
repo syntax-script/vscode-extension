d14c5c0887782898247e467cd24ef5753b90ce02
efekos <56752400+efekos@users.noreply.github.com>
2024-04-30 18:09:46 +0300

    Update initialize method
    Make initialize method use TextDocumentSyncKind.Full instead of 1

--- server/src/method/initialize.ts
+++ server/src/method/initialize.ts
@@ -1,4 +1,4 @@
-import { InitializeResult, RequestMessage } from 'lsp-types';
+import { InitializeResult, RequestMessage, TextDocumentSyncKind } from 'lsp-types';
 
 
 export function initialize(message: RequestMessage): InitializeResult {
@@ -6,7 +6,7 @@ export function initialize(message: RequestMessage): InitializeResult {
     return {
         capabilities: {
             completionProvider: {},
-            textDocumentSync: 1,
+            textDocumentSync: TextDocumentSyncKind.Full,
             diagnosticProvider: { workspaceDiagnostics: true, interFileDependencies: true, identifier: 'syntax-script' },
             codeActionProvider: true,
             hoverProvider: true,
